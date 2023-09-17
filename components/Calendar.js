86eb6c23328201c9c7b68520068e51bc519d9835
Syed Mohammad Younis <129735431+MuhammadYounus09@users.noreply.github.com>
2023-09-17 11:51:57 -0700

    index pages

--- components/Calendar.js
+++ components/Calendar.js
@@ -99,14 +99,18 @@ console.log(popoverOpen,draggedEventRef.current == info.event.id)
     setCurrentView(view);
   };
 
+  console.log({currentView})
   return (
     <>
       <div>
        <div>
-     
+       <select name="view-toggle" id="view-toggle">
+  <option value="Week View" onClick={()=>setCurrentView('timeGridWeek')}>Week View</option>
+  <option value="Month View" onClick={()=>setCurrentView('dayGridMonth')}>Month View</option>
+</select>
        </div>
         <FullCalendar
-          initialView={`dayGridMonth`}
+          initialView={currentView}
           plugins={[
             dayGridPlugin,
             interactionPlugin,
